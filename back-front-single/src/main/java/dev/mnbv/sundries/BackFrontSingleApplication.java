36071a227f5e6eac1d2376aa7059326cb28462ca
kim <kim@kimui-MacBookPro.local>
2025-01-24 13:30:38 +0900

    back-front-single-jar application sample

---
+++ back-front-single/src/main/java/dev/mnbv/sundries/BackFrontSingleApplication.java
@@ -0,0 +1,13 @@
+package dev.mnbv.sundries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackFrontSingleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackFrontSingleApplication.class, args);
+    }
+
+}
---
+++ back-front-single/src/test/java/dev/mnbv/sundries/BackFrontSingleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mnbv.sundries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackFrontSingleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
