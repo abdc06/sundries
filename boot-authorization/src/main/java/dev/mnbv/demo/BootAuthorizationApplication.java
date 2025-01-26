1d3e94f5b246735c31db507f7ff777cc95639eb4
kim <kim@kimui-MacBookPro.local>
2025-01-26 12:29:34 +0900

    init boot-authorization project

---
+++ boot-authorization/src/main/java/dev/mnbv/demo/BootAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package dev.mnbv.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootAuthorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootAuthorizationApplication.class, args);
+    }
+
+}
