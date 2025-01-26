cef23bab62387f024f2099acf02e74a22ccdfa73
kim <kim@kimui-MacBookPro.local>
2025-01-26 20:45:20 +0900

    google oauth2 적용 및 시크릿 키 env 처리

---
+++ boot-authorization/src/main/java/dev/mnbv/demo/common/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package dev.mnbv.demo.common.config;
+
+import dev.mnbv.demo.common.auth.service.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/private/**").authenticated()
+                        .anyRequest().permitAll())
+                .oauth2Login(oauth2LoginConfigurer -> oauth2LoginConfigurer
+                        .loginPage("/loginForm")
+                        .defaultSuccessUrl("/")
+                        .userInfoEndpoint(userInfoEndpointConfig ->
+                                userInfoEndpointConfig.userService(customOAuth2UserService))
+                ).build();
+    }
+}
